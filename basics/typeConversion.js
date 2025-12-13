5dab3a17a329e57dd2bdafb97cf1d2ff79bcc62a
sudhanshu tripathi <sudhanshutripathiaxis@gmail.com>
2025-12-13 06:07:59 +0000

    typeConversion completed

---
+++ basics/typeConversion.js
@@ -0,0 +1,18 @@
+let score = 40;
+
+console.log(score)// we are  printing here this score 
+
+// now wil c;heck the tyoe of this vaiable 
+
+console.log(typeof(score))// it will print number
+
+
+// now after creating a new variablle we wil  chaneg the type of that variable and we will parint the typpe 
+
+let age="50"
+console.log(typeof age)
+// it will print string 
+console.log(typeof Number(age));
+// now after conversion it will print number 
+console.log(age)
+console.log(typeof age)
