7c91c9a52d7db9c764c2c17377c6d44e30c4d74d
sudhanshu tripathi <sudhanshutripathiaxis@gmail.com>
2025-12-13 04:28:56 +0000

    varibles completed

---
+++ basics/variables.js
@@ -0,0 +1,25 @@
+const marks =30;
+var name ="karan"  // we will not use var because it isnot a scope variable 
+let age =40;// we will use this variable declaration .
+
+console.log (marks)
+
+console.log(name)
+
+console.log(age)
+
+
+// checking type of a varibale 
+
+console.log(typeof marks )
+// we can also check the type of variable using the typeof() mathod.
+
+console.log(typeof(marks))
+
+// if we arer not using a ";" in js then no problem . but we should follw a proper syntax.
+
+
+{
+    // this part is known as a scope.
+}
+
