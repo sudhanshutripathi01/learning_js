44d79e339b9216404985214780b39285ecd871e1
sudhanshu tripathi <sudhanshutripathiaxis@gmail.com>
2025-12-15 14:26:22 +0000

    comparesion

---
+++ basics/comparision.js
@@ -0,0 +1,8 @@
+// typescript does not allow to compare two different data types 
+
+console.log("2">1)
+console.log("02">1)
+/// point to noted 
+
+// comparisons convert a null value to a number treating it as 0
+c
