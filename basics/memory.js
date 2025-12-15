d663bcabe49dadbfa9a0c42054ab0f57617fbe11
sudhanshu tripathi <sudhanshutripathiaxis@gmail.com>
2025-12-15 15:16:54 +0000

    memory and string

---
+++ basics/memory.js
@@ -0,0 +1,21 @@
+//primitive data types are 7
+// string ,number , boolean,null,undefined, symbol,bigint 
+
+
+// there are also some non primitive data types 
+
+//array , object 
+
+
+
+// stack , heap 
+
+// stack is used in  primitive data types and feap is used in non jprimitive data types 
+
+let myname="hii this is me "
+
+
+let user={
+    email:"u;ser@gmail.com"
+    
+}
---
+++ basics/string.js
@@ -0,0 +1,24 @@
+let name="sudhanshu "
+let age=20;
+
+console.log(`hii my name is ${name} and my age is ${age}`)
+
+
+
+// we cann declare strings using string object also 
+
+let myname=new String('pankaj')
+console.log(myname)
+
+
+
+// numbers in js 
+
+let score = 300
+console.log(score)
+
+let newScore= new Number(300)
+console.log(newScore.toString().length)
+
+
+// in math abs() converts j-ve value to a +ve and + ve remains +ve
