658168e730be2e459d24a6d3ca5cf06ba95c3a23
sudhanshu tripathi <sudhanshutripathiaxis@gmail.com>
2025-12-29 08:00:18 +0000

    objexct and array complete

---
+++ basics/Array.js
@@ -0,0 +1,30 @@
+//arrays
+
+const array=[1,2,3,4]
+// console.log(array);
+// console.log(array[3]);
+
+
+// const array1= new Array(1,2,3,4);
+// console.log(array1);
+
+// array.push(6);
+// array.pop();
+// array.unshift(9)
+// array.shift()
+// array.shift()
+
+// console.log(array.includes(9));
+// console.log(array.indexOf(9));
+// console.log(array.indexOf(3));
+
+
+//  console.log("A",array);
+//  const myn1=array.slice(1,3);
+//  console.log(myn1);
+ 
+//  console.log("B",array);
+//  const myn2=array.splice(1,3);
+//  console.log(myn2);
+
+//  console.log("C",array
