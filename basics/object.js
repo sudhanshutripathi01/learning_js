94250a1f61248bc3a4cdcf52756dbd00988a4297
sudhanshu tripathi <sudhanshutripathiaxis@gmail.com>
2025-12-30 03:28:00 +0000

    ks

--- basics/object.js
+++ basics/object.js
@@ -1,10 +1,22 @@
+const mysym=Symbol("my key 1")
+
+
+
 
 const jsuser ={
     name:"sudhanshu ",
+    [mysym]:"my key 1",
     age:21,
     email:"sudhanshutripathi@gmail.com"
 
 }
+console.log(jsuser);
 
 
 console.log(jsuser.email);
+console.log(jsuser["email"]);
+
+
+
+
+
