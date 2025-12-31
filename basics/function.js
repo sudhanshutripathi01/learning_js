127c209d9289d51ed6b15fc97d4b5223078ff2a0
sudhanshu tripathi <sudhanshutripathiaxis@gmail.com>
2025-12-31 08:13:35 +0000

    function

---
+++ basics/function.js
@@ -0,0 +1,22 @@
+//objects inn javascript are container of properties and methods
+//json is a data interchange formatwe that is used to trannsmit data beween servr and client.
+//ex of json
+// {
+//     "name":"jhon",
+//     "age":30,
+//     "city":"new york"
+// }
+
+
+// function is block of code that is used to perform a specific task 
+//function is reusable 
+//feature of function 
+//1.reusable
+//2.modularity
+//3.easy to maintain
+//4.readability
+
+//method is a function that is associated with an object
+
+
+//closeure is a function that has acccess to the parent scope, even after the parent funcion has closed 
